2ea6c9ae1662becdf1f2acc0ab6f40ee1c452552
Shodai Shibano <nameless.liberty.382@gmail.com>
2019-09-20 12:00:00 +0900

    koaksa

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :books
 resources :users
-
 get '/home/about'  => 'homes#about'
-
 root 'item/index'
-
-
 end
